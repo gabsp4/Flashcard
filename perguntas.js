3e521edf9a49d920ef022407d72aab5737260616
gabsp4 <150672639+gabsp4@users.noreply.github.com>
2024-10-11 08:01:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação',
+    'o que é Phyton?',
+    'O  Phyton é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Capital da frança?',
+    'Capital da frança é Paris'
+)
+
+criaCartao(
+    'Programação 2.0',
+    'o que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
